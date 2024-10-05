767b6249d35877091ab85904f0c8e105ac3db229
ralph12322 <154263335+ralph12322@users.noreply.github.com>
2024-10-05 16:17:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+document.getElementById('flipButton').addEventListener('click', function() {
+    const flipResult = Math.random() < 0.5 ? 'Heads' : 'Tails';
+
+    const imgResult = flipResult === 'Heads' 
+        ? 'https://img.freepik.com/premium-vector/kangaroo-head_1095437-4322.jpg' 
+        : 'https://e7.pngegg.com/pngimages/441/245/png-clipart-kangaroo-cartoon-drawing-kangaroo-mammal-animals.png';
+
+    document.getElementById('result').innerText = `Result: ${flipResult}`;
+
+    const existingImage = document.getElementById('resultImage');
+    if (existingImage) {
+        existingImage.remove();
+    }
+
+    const imgElement = document.createElement('img');
+    imgElement.src = imgResult;
+    imgElement.alt = flipResult; 
+    imgElement.id = 'resultImage'; 
+    document.body.appendChild(imgElement);
+});
+
+
